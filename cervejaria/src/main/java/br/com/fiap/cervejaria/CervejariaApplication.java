20a49b58f0bb24076bbee693d20e3a51bf504c69
leorvieira <leonardorangelvieira@gmail.com>
2020-01-26 23:14:01 -0200

    atualizacao 1

--- cervejaria/src/main/java/br/com/fiap/cervejaria/CervejariaApplication.java
+++ cervejaria/src/main/java/br/com/fiap/cervejaria/CervejariaApplication.java
@@ -1,16 +1,13 @@
 package br.com.fiap.cervejaria;
 
-import br.com.fiap.cervejaria.dto.Tipo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class CervejariaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("passou no main ");
 		SpringApplication.run(CervejariaApplication.class, args);
 	}
 
--- cervejaria/src/main/java/br/com/fiap/cervejaria/Controller/CervejaController.java
+++ cervejaria/src/main/java/br/com/fiap/cervejaria/Controller/CervejaController.java
@@ -2,7 +2,6 @@ package br.com.fiap.cervejaria.Controller;
 
 import br.com.fiap.cervejaria.dto.CervejaDTO;
 import br.com.fiap.cervejaria.dto.Tipo;
-import org.omg.CosNaming.NamingContextPackage.NotFound;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -11,10 +10,10 @@ import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @RestController
-@RequestMapping("cervejas")
+@RequestMapping(value = "cervejas/leo")
 public class CervejaController {
 
 
@@ -22,38 +21,70 @@ public class CervejaController {
 
     public CervejaController(){
 
+        System.out.println("passou por aqui ?");
         cervejaDTOList = new ArrayList<>();
 
         cervejaDTOList.add( new CervejaDTO(1, "Marca1", 3.5,Tipo.ALE, new BigDecimal(17.2), ZonedDateTime.now().minusYears(1)));
+        cervejaDTOList.add( new CervejaDTO(2, "Marca2", 3.5,Tipo.WEISS, new BigDecimal(17.9), ZonedDateTime.now().minusYears(1)));
+    }
+
+/*
+    @GetMapping
+    public String hello(@RequestParam Integer id){
+
+        return ("Hello World - > "+id);
 
     }
 
-
- /*   @GetMapping
-    public String hello(){
-        return "Hello World";
-
-    }
-*/
     @GetMapping
     public List<CervejaDTO> getAll(@RequestParam Tipo tipo){
+
         return cervejaDTOList.stream()
                 .filter(cervejaDTO -> cervejaDTO.getTipo().equals(tipo))
                 .collect(Collectors.toList());
 
     }
+*/
+    @GetMapping("all")
+    public List getAll(){
+        System.out.println("PASSOU AQUI");
+        return cervejaDTOList;
 
+    }
+
+/*
     @GetMapping
     public CervejaDTO findById(@PathVariable Integer id){
+
+        System.out.println("hahahah");
         return cervejaDTOList.stream()
                 .filter(cervejaDTO -> cervejaDTO.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+
+*/
+
+    @GetMapping
+    @RequestMapping(method = RequestMethod.GET)
+    public CervejaDTO findById(Integer id){
+
+        System.out.println("hahahah");
+        return cervejaDTOList.stream()
+                .filter(cervejaDTO -> cervejaDTO.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+    }
+
+
     @PostMapping
+    @RequestMapping(method = RequestMethod.POST)
+
     public CervejaDTO create(@RequestBody CreateCervejaDTO createCervejaDTO){
 
+        System.out.println("fffffff");
         CervejaDTO cervejaDTO = new CervejaDTO( createCervejaDTO, cervejaDTOList.size() + 1);
         cervejaDTOList.add(cervejaDTO);
         return cervejaDTO;
@@ -72,16 +103,17 @@ public class CervejaController {
 
         return  cervejaDTO;
         }
-
- /*       @PatchMapping("{id}")
+/*
+       @PatchMapping("{id}")
         public CervejaDTO update(@PathVariable Integer id, @RequestBody PrecoCervejaDTO precoCervejaDTO){
-
 
         }
 */
 
-    @DeleteMapping("{id}")
+     @DeleteMapping("{id}")
     public void detete(@PathVariable Integer id){
+
+        System.out.println("hehehe");
         CervejaDTO cervejaDTO = findById(id);
         cervejaDTOList.remove(cervejaDTO);
     }
